d048e9258d8402e0ac6d7182bff65d25c9652d13
Webdream27 <andreamariasigl@gmail.com>
2025-05-08 15:02:52 +0200

    Add files via upload

---
+++ zahlen_nebeneinander_aufgabe_zwei.py
@@ -0,0 +1,17 @@
+"""************************************************
+Einsendeaufgabe 3.2
+***********************************************"""
+
+# Einlesen des Wertes
+zahl = 1
+
+
+# Ausgabe des Wertes
+while zahl <= 50:
+    if zahl < 50:
+        print(zahl, end=",")
+    else:
+        print(zahl)
+        
+    zahl = zahl + 1
+
